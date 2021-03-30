a52de0d7bc85dc77fc4361f35a47cd43d5bd7e5a
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-03-30 22:16:38 +0700

    Create MainViewModelFactory class

---
+++ app/src/main/java/com/rujirakongsomran/kt_retrofit2/MainViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.rujirakongsomran.kt_retrofit2
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.rujirakongsomran.kt_retrofit2.repository.Repository
+
+class MainViewModelFactory(
+    private val repository: Repository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
+
+}
